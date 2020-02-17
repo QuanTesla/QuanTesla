a171a4150edf67fae5cf16ee487e302013f8a537
Brian <CyberMobius@users.noreply.github.com>
2020-02-16 19:26:10 -0500

    Added candlestick graph

--- services/data_processor/src/StockDataPipe.py
+++ services/data_processor/src/StockDataPipe.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 
 # Wrapper to get a dataframe for the financial history of a company
@@ -8,15 +9,26 @@ def create_stock_data(company: str, period: str = "1y", interval: str = "1d") ->
     return yf.Ticker(company).history(period, interval).T.transpose()
 
 
+# Wrapper to create a candlestick graph
+def create_candlestick_graph(stock_data: pd.DataFrame) -> go.Candlestick:
+    return go.Figure(data=[go.Candlestick(
+        x=stock_data.index,
+        open=stock_data.Open,
+        low=stock_data.Low,
+        high=stock_data.High,
+        close=stock_data.Close
+        )])
+
+
 if __name__ == "__main__":
     # Fetches financial data from Tesla's stock price each day in the last year.
     # The .T fetches the dataframe for the data and transposing it makes it easier
     # to work with as that way you can have named columns
-    tsla = yf.Ticker("TSLA").history(period="1y", interval="1d").T.transpose()
+    tsla = create_stock_data(company="TSLA", period="1y", interval="1d")
 
     # This creates the graph where the x axis is the datetime of the graph and the y
     # axis is the data from the open column
-    fig = px.line(tsla, x=tsla.index, y="Open")
+    fig = create_candlestick_graph(tsla)
 
     # Show the data
     fig.show()
